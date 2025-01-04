78d8619da72ef73221b301f0ad05c225c2466d0b
징힌으 <145961554+wkdgusdn0106@users.noreply.github.com>
2025-01-04 10:24:32 +0900

    [Platinum IV] Title: 불 끄기, Time: 204 ms, Memory: 112760 KB -BaekjoonHub

--- 백준/Platinum/14939. 불 끄기/불 끄기.py
+++ 백준/Platinum/14939. 불 끄기/불 끄기.py
@@ -1,37 +1,35 @@
-import copy
-aa = [list(map(lambda x: False if x == '#' else True,input())) for i in range(10)]
+bulb = [list(map(lambda x: x == 'O',input())) for i in range(10)]
 ans = float('inf')
-m = []
-for j in range(0,1<<10):
-    a = copy.deepcopy(aa)
+
+for j in range(1<<10):
+    bulbs = [row[:] for row in bulb]
     binary = list(map(int,bin(j)[2:].zfill(10)))
     cnt = 0
-    n=[]
+    
+    #첫 번째 줄
     for k in range(10):
         if binary[k]:
             if k > 0:
-                a[0][k-1] = not(a[0][k-1])
+                bulbs[0][k-1] ^= 1
             if k < 9:
-                a[0][k+1] = not(a[0][k+1])
-            a[0][k] = not(a[0][k])
-            a[1][k] = not(a[1][k])
-            cnt+=1
-            n.append((k))
+                bulbs[0][k+1] ^= 1
+            bulbs[0][k] ^= 1
+            bulbs[1][k] ^= 1
+            cnt += 1
+    
+    #그 밑에꺼
     for k in range(1,10):
         for i in range(10):
-            if a[k-1][i]:
-                a[k-1][i] = False
-                a[k][i] = not(a[k][i])
+            if bulbs[k-1][i]:
+                bulbs[k-1][i] ^= 1
+                bulbs[k][i] ^= 1
                 if i > 0:
-                    a[k][i-1] = not(a[k][i-1])
+                    bulbs[k][i-1] ^= 1
                 if i < 9:
-                    a[k][i+1] = not(a[k][i+1])
+                    bulbs[k][i+1] ^= 1
                 if k < 9:
-                    a[k+1][i] = not(a[k+1][i])
+                    bulbs[k+1][i] ^= 1
                 cnt+=1
-                n.append((k,i))
-    if True not in a[-1]:
-        if ans > cnt:
-            m = n
+    if not any(bulbs[-1]):
         ans = min(ans,cnt)
 print(ans)
