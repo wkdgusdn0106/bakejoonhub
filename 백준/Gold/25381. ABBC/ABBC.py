36f14a702b7767786ede04ab58736ad4b3879472
징힌으 <145961554+wkdgusdn0106@users.noreply.github.com>
2025-01-12 11:30:15 +0900

    [Gold III] Title: ABBC, Time: 128 ms, Memory: 121148 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/25381. ABBC/ABBC.py
+++ 백준/Gold/25381. ABBC/ABBC.py
@@ -1,24 +1,31 @@
 a = list(input())
-cnt = 0
-b = []
+acnt = 0
+bcnt = 0
+ccnt = 0
+i = 0
+ab = 0
+bc =0
 for i in range(len(a)):
-    put = 1
+    if a[i] == 'A':
+        acnt += 1
+        bcnt = 0
+    elif a[i] == 'B':
+        bcnt += 1
+        if acnt >= bcnt and acnt != 0:
+            ab += 1
+            mi = min(acnt,bcnt)
+            acnt -= mi
+            bcnt -= mi
+bcnt = 0
+for i in range(len(a)):
     if a[i] == 'B':
-        for j in range(i+1,len(a)):
-            if a[j] == 'C':
-                cnt += 1
-                put = 0
-                a[j] = 0
-                break
-    elif a[i] == 0:
-        put = 0
-    if put:
-        b.append(a[i])
-for i in range(len(b)):
-    if b[i] == 'A':
-        for j in range(i+1,len(b)):
-            if b[j] == 'B':
-                cnt += 1
-                b[j] = 0
-                break
-print(cnt)
+        bcnt += 1
+        ccnt = 0
+    elif a[i] == 'C':
+        ccnt += 1
+        if ccnt <= bcnt and bcnt != 0:
+            bc += 1
+            mi = min(ccnt,bcnt)
+            ccnt -= mi
+            bcnt -= mi
+print(min(a.count('B'), ab+bc))
