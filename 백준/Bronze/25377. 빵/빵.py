4946e45e50ea69b52afd78cb486d051a83a444a9
징힌으 <145961554+wkdgusdn0106@users.noreply.github.com>
2025-01-26 09:54:18 +0900

    [Bronze IV] Title: 빵, Time: 92 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -3,5 +3,5 @@ ans = float('inf')
 for i in range(a):
     b,c = map(int,input().split())
     if b <= c:
-        ans = max(ans,c)
+        ans = min(ans,c)
 print(-1 if ans == float('inf') else ans)
