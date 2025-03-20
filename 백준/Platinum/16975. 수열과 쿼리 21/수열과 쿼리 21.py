3ac8d815e889e8b6b98873134d65ebe594ba7c75
징힌으 <145961554+wkdgusdn0106@users.noreply.github.com>
2025-03-20 22:20:48 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 2432 ms, Memory: 129860 KB -BaekjoonHub

---
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.py
@@ -0,0 +1,53 @@
+def init(n):
+    global tree,lazy
+    tree = [0]*(4*n)
+    lazy = [0]*(4*n)
+def build(node,start,end, arr):
+    if start == end:
+        tree[node] = arr[start]
+        return
+    mid = (start+end) // 2
+    build(node*2, start,mid, arr)
+    build(node*2+1, mid+1, end, arr)
+    tree[node] = tree[node*2] + tree[node*2+1]
+    
+def propagate(node, start, end):
+    if lazy[node] != 0:
+        tree[node] += (end-start+1) * lazy[node]
+        if start != end:
+            lazy[node*2] += lazy[node]
+            lazy[node*2+1] += lazy[node]
+        lazy[node] = 0
+
+def update_range(node, start, end, l, r, value):
+    propagate(node,start,end)
+    if start > r or end < l:
+        return
+    if l <= start and end <= r:
+        lazy[node] += value
+        propagate(node, start, end)
+        return
+    mid = (start + end) // 2
+    update_range(node*2, start, mid, l, r, value)
+    update_range(node*2+1, mid+1, end, l ,r, value)
+    tree[node] = tree[node*2] + tree[node*2+1]
+
+def query_range(node, start, end, l, r):
+    propagate(node, start, end)
+    if start > r or end < l:
+        return 0
+    if l <= start and end <= r:
+        return tree[node]
+    mid = (start + end) // 2
+    return query_range(node*2, start, mid, l, r) + query_range(node*2+1, mid+1,end, l,  r)
+n = int(input())
+init(n)
+arr = list(map(int,input().split()))
+build(1, 0, n-1, arr)
+m = int(input())
+for i in range(m):
+    a = list(map(int,input().split()))
+    if a[0] == 1:
+        update_range(1, 0, n - 1, a[1] - 1, a[2] - 1, a[3])
+    else:
+        print(query_range(1, 0, n - 1, a[1] - 1, a[1] - 1))
